58879acb6b39ef1a227516e3a005a629023279e3
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-12-30 11:01:01 +0600

    Adding Main Controller

--- src/main/java/com/thikthak/app/main/MainController.java
+++ src/main/java/com/thikthak/app/main/MainController.java
@@ -3,12 +3,13 @@ package com.thikthak.app.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/main")
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "view/main/main";
     }
